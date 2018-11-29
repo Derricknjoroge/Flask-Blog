96fb1e862b1fb2af8474a0136becdc5dc75ac079
Derrick Njoroge <derricknjorog@gmail.com>
2018-11-29 13:44:16 +0300

    Added the layout template and linked it to the main pages

--- blog.py
+++ blog.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Derrick Njoroge',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+    },
+    {
+        'author': 'Ian Mbuti',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+    }
+]
 
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', all_posts=posts)
 
 
 @app.route("/about")
